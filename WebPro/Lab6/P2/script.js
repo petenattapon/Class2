63b5cffa3ebadaf073bcffc327c8990770229e40
petenattapon <nattaponpongkaop@gmail.com>
2023-04-19 15:45:15 +0700

    ยังไม่สมบูรณ์

---
+++ WebPro/Lab6/P2/script.js
@@ -0,0 +1,21 @@
+﻿function clicked() {
+    let button = document.querySelector('button');
+    let ul = document.querySelector('ul')
+
+    function changeText() { 
+      if (button.textContent === ">> Show details") {
+        button.textContent = "  Hide details";
+        ul.style.display = 'block'
+      } else {
+        button.textContent = ">> Show details";
+        ul.style.display = 'none'
+      }
+      button.removeEventListener('click', changeText);
+
+    }
+
+
+    button.addEventListener('click', changeText);
+
+    
+}
