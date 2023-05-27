9c63142f722e2b2325e66223c141c20cf271d2b9
petenattapon <nattaponpongkaop@gmail.com>
2023-05-27 22:03:36 +0700

    Edit Main

--- Network/Main.java
+++ Network/Main.java
@@ -1,101 +1,3 @@
-// import java.net.InetAddress;
-// import java.net.UnknownHostException;
-// import java.util.*;
-
-// class Node {
-//     private int cityCode;
-//     private List<Node> links;
-//     private String ipAddress; // เพิ่มตัวแปรเก็บ IP address
-
-//     public Node(int cityCode) {
-//         this.cityCode = cityCode;
-//         this.links = new ArrayList<>();
-//         this.ipAddress = getIPAddress(); // กำหนดค่า IP address เมื่อสร้าง Node
-//     }
-
-//     public int getCityCode() {
-//         return cityCode;
-//     }
-
-//     public List<Node> getLinks() {
-//         return links;
-//     }
-
-//     public void addLink(Node node) {
-//         links.add(node);
-//     }
-
-//     public String getIPAddress() {
-//         try {
-//             return InetAddress.getLocalHost().getHostAddress();
-//         } catch (UnknownHostException e) {
-//             e.printStackTrace();
-//             return "";
-//         }
-//     }
-
-//     public String getFormattedIPAddress() {
-//         return ipAddress.replace("/", "");
-//     }
-// }
-
-// class TSPSolver {
-//     public void solveTSP(Node startNode) {
-//         Queue<Node> queue = new LinkedList<>();
-//         Set<String> visited = new HashSet<>();
-
-//         queue.add(startNode);
-
-//         while (!queue.isEmpty()) {
-//             Node currentNode = queue.poll();
-//             int cityCode = currentNode.getCityCode();
-
-//             if (!visited.contains(Integer.toString(cityCode))) {
-//                 System.out.println("City " + cityCode + " (IP: " + currentNode.getFormattedIPAddress() + ") send " + cityCode + " to all Cities");
-
-//                 for (Node nextNode : currentNode.getLinks()) {
-//                     String nextCityCode = Integer.toString(nextNode.getCityCode());
-//                     String combinedCode = Integer.toString(cityCode) + nextCityCode;
-
-//                     if (!visited.contains(combinedCode)) {
-//                         System.out.println("City " + nextCityCode + " (IP: " + nextNode.getFormattedIPAddress() + ") received and added " + nextCityCode + " to become " + combinedCode);
-//                         visited.add(combinedCode);
-//                         queue.add(nextNode);
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// public class Main {
-//     public static void main(String[] args) {
-//         Node node1 = new Node(1);
-//         Node node2 = new Node(2);
-//         Node node3 = new Node(3);
-//         Node node4 = new Node(4);
-
-//         node1.addLink(node2);
-//         node1.addLink(node3);
-//         node1.addLink(node4);
-
-//         node2.addLink(node1);
-//         node2.addLink(node3);
-//         node2.addLink(node4);
-
-//         node3.addLink(node1);
-//         node3.addLink(node2);
-//         node3.addLink(node4);
-
-//         node4.addLink(node1);
-//         node4.addLink(node2);
-//         node4.addLink(node3);
-
-//         TSPSolver tspSolver = new TSPSolver();
-//         tspSolver.solveTSP(node1);
-//     }
-// }
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.*;
@@ -152,14 +54,14 @@ class TSPSolver {
             int cityCode = currentNode.getCityCode();
 
             if (!visited.contains(Integer.toString(cityCode))) {
-                String result = "City " + cityCode + " (IP: " + currentNode.getFormattedIPAddress() + ") send " + cityCode + " to all Cities";
-
+                String result = "City " + cityCode + " sends " + cityCode + " to all cities: ";
                 for (Node nextNode : currentNode.getLinks()) {
+                    result += nextNode.getCityCode() + ", ";
+
                     String nextCityCode = Integer.toString(nextNode.getCityCode());
                     String combinedCode = Integer.toString(cityCode) + nextCityCode;
 
                     if (!visited.contains(combinedCode)) {
-                        result += "\nCity " + nextCityCode + " (IP: " + nextNode.getFormattedIPAddress() + ") received and added " + nextCityCode + " to become " + combinedCode;
                         visited.add(combinedCode);
                         queue.add(nextNode);
                     }
@@ -200,9 +102,137 @@ public class Main {
         TSPSolver tspSolver = new TSPSolver();
         String[] results = tspSolver.solveTSP(node1);
 
-        // พิมพ์ผลลัพธ์ของแต่ละ Node
+        // แสดงผลลัพธ์ของแต่ละโหนด
         for (int i = 0; i < results.length; i++) {
-            System.out.println("Result for Node " + (i + 1) + ":\n" + results[i]);
+            System.out.println("Result for Node " + (i + 1) + ":");
+            System.out.println(results[i]);
         }
     }
 }
+
+
+
+
+// import java.net.InetAddress;
+// import java.net.UnknownHostException;
+// import java.util.*;
+
+// class Node {
+//     private int cityCode;
+//     private List<Node> links;
+//     private String ipAddress; // เพิ่มตัวแปรเก็บ IP address
+
+//     public Node(int cityCode) {
+//         this.cityCode = cityCode;
+//         this.links = new ArrayList<>();
+//         this.ipAddress = getIPAddress(); // กำหนดค่า IP address เมื่อสร้าง Node
+//     }
+
+//     public int getCityCode() {
+//         return cityCode;
+//     }
+
+//     public List<Node> getLinks() {
+//         return links;
+//     }
+
+//     public void addLink(Node node) {
+//         links.add(node);
+//     }
+
+//     public String getIPAddress() {
+//         try {
+//             return InetAddress.getLocalHost().getHostAddress();
+//         } catch (UnknownHostException e) {
+//             e.printStackTrace();
+//             return "";
+//         }
+//     }
+
+//     public String getFormattedIPAddress() {
+//         return ipAddress.replace("/", "");
+//     }
+// }
+
+// class TSPSolver {
+//     public String[] solveTSP(Node startNode) {
+//         Queue<Node> queue = new LinkedList<>();
+//         Set<String> visited = new HashSet<>();
+
+//         queue.add(startNode);
+
+//         String[] results = new String[4];
+
+//         while (!queue.isEmpty()) {
+//             Node currentNode = queue.poll();
+//             int cityCode = currentNode.getCityCode();
+
+//             if (!visited.contains(Integer.toString(cityCode))) {
+//                 StringBuilder result = new StringBuilder();
+//                 result.append("City ")
+//                         .append(cityCode)
+//                         .append(" (IP: ")
+//                         .append(currentNode.getFormattedIPAddress())
+//                         .append(") send ")
+//                         .append(cityCode)
+//                         .append(" to all Cities");
+
+//                 for (Node nextNode : currentNode.getLinks()) {
+//                     String nextCityCode = Integer.toString(nextNode.getCityCode());
+//                     String combinedCode = Integer.toString(cityCode) + nextCityCode;
+
+//                     if (!visited.contains(combinedCode)) {
+//                         result.append("\nCity ")
+//                                 .append(nextCityCode)
+//                                 .append(" (IP: ")
+//                                 .append(nextNode.getFormattedIPAddress())
+//                                 .append(") received and added ")
+//                                 .append(nextCityCode)
+//                                 .append(" to become ")
+//                                 .append(combinedCode);
+//                         visited.add(combinedCode);
+//                         queue.add(nextNode);
+//                     }
+//                 }
+
+//                 results[cityCode - 1] = result.toString();
+//             }
+//         }
+
+//         return results;
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Node node1 = new Node(1);
+//         Node node2 = new Node(2);
+//         Node node3 = new Node(3);
+//         Node node4 = new Node(4);
+
+//         node1.addLink(node2);
+//         node1.addLink(node3);
+//         node1.addLink(node4);
+
+//         node2.addLink(node1);
+//         node2.addLink(node3);
+//         node2.addLink(node4);
+
+//         node3.addLink(node1);
+//         node3.addLink(node2);
+//         node3.addLink(node4);
+
+//         node4.addLink(node1);
+//         node4.addLink(node2);
+//         node4.addLink(node3);
+
+//         TSPSolver tspSolver = new TSPSolver();
+//         String[] results = tspSolver.solveTSP(node1);
+
+//         // Print the results for each node
+//         for (int i = 0; i < results.length; i++) {
+//             System.out.println("Result for Node " + (i + 1) + ":\n" + results[i]);
+//         }
+//     }
+// }
+
